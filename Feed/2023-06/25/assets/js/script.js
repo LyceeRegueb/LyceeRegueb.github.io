39501c175f3716342e8c8e95a91edf0c7cc28a6f
Kahli Mohamed Nour <kahlimohamednour@gmail.com>
2023-06-25 10:56:07 -0700

    main

---
+++ Feed/2023-06/25/assets/js/script.js
@@ -0,0 +1,24 @@
+$(function() {
+    $('.level-slider').slick({
+        dots: true,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear'
+      });
+});
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader");
+  
+    loader.classList.add("loader--hidden");
+  
+    loader.addEventListener("transitionend", () => {
+      document.body.removeChild(loader);
+    });
+  });
+
+
+
+
+
+  
